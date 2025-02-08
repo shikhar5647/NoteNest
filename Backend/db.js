90a8b8c8974fff4387d2d0c425c003989f30220f
Shikhar Dave <b22ch032@iitj.ac.in>
2025-02-09 01:20:32 +0530

    ignus

--- Backend/db.js
+++ Backend/db.js
@@ -1,2 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/NoteNest';
+const mongoURI = 'mongodb://localhost:27017/notenest';
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Connected to Mongo Successfully");
+    })
+}
+
+module.exports = connectToMongo;
