909039e5c8c3b5b8a629e7d7095b3535f055f9f3
Shikhar Dave <b22ch032@iitj.ac.in>
2025-02-09 10:32:16 +0530

    debug

--- Backend/db.js
+++ Backend/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = async () => {
-    try {
-        await mongoose.connect(' mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Connected to MongoDB successfully");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1);  // Exit the process if connection fails
-    }
+  try {
+    await mongoose.connect('mongodb://localhost:27017/notenest', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('✅ Connected to MongoDB successfully');
+  } catch (error) {
+    console.error('❌ Error connecting to MongoDB:', error);
+  }
 };
 
 module.exports = connectToMongo;
+
