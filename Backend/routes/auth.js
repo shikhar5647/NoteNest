1e7ec3194a13b761a60565507bff52ec7732a8a3
Shikhar Dave <b22ch032@iitj.ac.in>
2025-02-09 12:41:04 +0530

    express_validator

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router=express.Router();
 const {Schema,model}=require('mongoose');
 const User=require('../models/User');
+const {body,validationResult}=require('express-validator');
 
 
 //
 
-router.get('/',(req,res)=>{
+router.get('/',[
+    body('name','Enter a valid name').isLength({min:3}),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password must be atleast 5 characters').isLength({min:5}),
+],
+    (req,res)=>{
     console.log(req.body);
     const user=User(req.body);
     user.save();
