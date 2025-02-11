dae53a60bba9d58f43014283ab73dec505b87d08
Shikhar Dave <b22ch032@iitj.ac.in>
2025-02-11 20:37:46 +0530

    auth

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,31 +1,28 @@
 const express = require('express');
-const router=express.Router();
-const {Schema,model}=require('mongoose');
-const User=require('../models/User');
-const {body,validationResult}=require('express-validator');
+const router = express.Router();
+const {Schema,model} = require('mongoose');
+const User = require('../models/User');
+const {body,validationResult} = require('express-validator');
 
-
-//
-
-router.get('/',[
+router.post('/', [
     body('name','Enter a valid name').isLength({min:3}),
     body('email','Enter a valid email').isEmail(),
     body('password','Password must be atleast 5 characters').isLength({min:5}),
 ],
-    (req,res)=>{
-    const errors=validationResult(req);
+(req,res) => {
+    const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors:errors.array()});
     }
     User.create({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password,
-    }).then(user=>res.json(user))
-    .catch(err=>{
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err => {
         console.log(err);
         res.json({error:"Some error occured"});
     })
-})
+});
 
-module.exports=router;
+module.exports = router;
