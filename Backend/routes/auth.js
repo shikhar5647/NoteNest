1b05f014918829d120142892089c28f7f3fdfd86
Shikhar Dave <b22ch032@iitj.ac.in>
2025-02-12 18:32:32 +0530

    chai

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -70,5 +70,13 @@ async (req,res) => {
                 id:user.id
             }
         }
-
+        const authToken = jwt.sign(payload,JWT_SECRET);
+        res.json({authToken});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
+}
+)
 module.exports = router;
