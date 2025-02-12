144e9e0cddbffc1abd902dbab68cafb82d3ac452
Shikhar Dave <b22ch032@iitj.ac.in>
2025-02-12 09:49:14 +0530

    auth_better

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const database = connectToMongo(); // Ensure this is called before starting the server
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,7 +4,8 @@ const {Schema,model} = require('mongoose');
 const User = require('../models/User');
 const {body,validationResult} = require('express-validator');
 
-router.post('/', [
+// Create a new user using : POST "/api/auth/createuser". No login required
+router.post('/createuser', [
     body('name','Enter a valid name').isLength({min:3}),
     body('email','Enter a valid email').isEmail(),
     body('password','Password must be atleast 5 characters').isLength({min:5}),
@@ -15,21 +16,29 @@ async (req,res) => {
     if(!errors.isEmpty()){
         return res.status(400).json({errors:errors.array()});
     }
-    // check whether the user with email already exits
-    let user = await User.findOne({email: req.body.email});
-    if(user){
-        return res.status(400).json({error: "Sorry a user with this email already exists"});
+    try{
+        // check whether the user with email already exits
+        let user = await User.findOne({email: req.body.email});
+        console.log(user);
+        if(user){
+            return res.status(400).json({error: "Sorry a user with this email already exists"});
+        }
+        user= await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        // .then(user => res.json(user))
+        // .catch(err => {
+        //     console.log(err);
+        //     res.json({error:"Some error occured"});
+        // })
+        res.json(user);
     }
-    user= await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    })
-    // .then(user => res.json(user))
-    // .catch(err => {
-    //     console.log(err);
-    //     res.json({error:"Some error occured"});
-    // })
+    catch(error){
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 module.exports = router;
