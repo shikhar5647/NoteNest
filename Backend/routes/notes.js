01e6796f9129b76d1eaa860baa5c25cbdb305247
Shikhar Dave <b22ch032@iitj.ac.in>
2025-03-10 00:22:16 +0530

    update

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -26,4 +26,28 @@ router.post('/addnote', fetchuser, [
             res.status(500).send("Internal Server Error");
         }
 })
+//ROUTE 3: Update an existing Note using: PUT "/api/notes/updatenote". Login required
+
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    try {
+        //Create a newNote object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+        //Find the note to be updated and update it
+        let note = await Notes.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+        note= await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})  
 module.exports = router
