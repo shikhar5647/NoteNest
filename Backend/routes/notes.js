5233698a1f5869b4a03482225abb8c38e0190974
Shikhar Dave <b22ch032@iitj.ac.in>
2025-03-09 23:47:00 +0530

    add_notes

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -4,10 +4,26 @@ var fetchuser = require('../middleware/fetchuser');
 const Notes = require('../models/Notes');
 
 //ROUTE 1: Get All the Notes using: GET "/api/notes/fetchallnotes". Login required
-router.get('/fetchallnotes', fetchuser, async(req, res) => {
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
     const notes = Notes.find({ user: req.user.id });
     res.json(notes);
-});
-
-
-module.exports = router;
+})
+//ROUTE 2: Add a new Note using: POST "/api/notes/addnote". Login required
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 })],
+    async (req, res) => {
+        try {
+            const { title, description, tag } = req.body;
+            const note = new Notes({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save();
+            res.json(savedNote)
+        }
+        catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+})
+module.exports = router
