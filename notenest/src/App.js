91aac56116c31325eaf22651a8df5f5f0f12f457
Shikhar Dave <b22ch032@iitj.ac.in>
2025-03-20 19:23:19 +0530

    navbar

--- notenest/src/App.js
+++ notenest/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <h1> This is Notenest </h1>
-    </div>
+    </>
   );
 }
 
--- notenest/src/Components/Home.js
+++ notenest/src/Components/Home.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
 
 export const Home = () => {
   return (
