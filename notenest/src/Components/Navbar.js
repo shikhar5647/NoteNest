b99d9e14cd7eda5b95cbcb2778efe67825ebb9c8
Shikhar Dave <b22ch032@iitj.ac.in>
2025-03-21 18:28:28 +0530

    active

--- notenest/src/Components/Navbar.js
+++ notenest/src/Components/Navbar.js
@@ -5,12 +5,18 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 import About from './About';
+import { useEffect } from 'react';
 
 const Navbar = () => {
+    let location = useLocation();
+    useEffect(() => {
+        console.log(location.pathname);
+    }, [location]);
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">Notenest</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,10 +25,10 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                <Link className={`nav-link ${location.pathname==="/"? "active":""}`} aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/About">About</Link>
+                                <Link className={`nav-link ${location.pathname==="/About"? "active":""}`} to="/About">About</Link>
                             </li>
                             
                         </ul>
