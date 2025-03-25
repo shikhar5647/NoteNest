f17ac2557f6f91fb0fb34de9ecc72842d9718b0c
Shikhar Dave <b22ch032@iitj.ac.in>
2025-03-26 01:20:57 +0530

    modal

--- notenest/src/Components/Notes.js
+++ notenest/src/Components/Notes.js
@@ -1,19 +1,49 @@
-import React,{useContext} from 'react';
+import React, { useContext, useEffect } from 'react';
 import Noteitem from './Noteitem';
 import AddNote from './AddNote';
 
 import noteContext from '../Context/notes/noteContext';
 const Notes = () => {
   const context = useContext(noteContext);
-  const {notes, getNotes} = context;
+  const { notes, getNotes } = context;
+  useEffect(() => {
+    getNotes();
+    // eslint-disable-next-line
+  }, []);
+  
   return (
-    <div className='row my-3'>
+    <>
+      <AddNote />
+      {/* Button trigger modal */}
+      <button type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+      {/* Modal */}
+      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='row my-3'>
         <h2>Your Notes</h2>
         {notes.map((note) => {
           return <Noteitem key={note._id} note={note} />
         })}
-      
-    </div>
+
+      </div>
+    </>
   );
 };
 
