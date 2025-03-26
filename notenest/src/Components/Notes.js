fcb954a923d85403e50fc425602b9c079cfce806
Shikhar Dave <b22ch032@iitj.ac.in>
2025-03-26 10:42:02 +0530

    added_form

--- notenest/src/Components/Notes.js
+++ notenest/src/Components/Notes.js
@@ -19,6 +19,9 @@ const Notes = () => {
   return (
     <>
       <AddNote />
+      <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
       {/* Modal */}
       <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog">
@@ -28,11 +31,27 @@ const Notes = () => {
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">Title</label>
+                  <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" />
+                  <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="description" className="form-label">Description</label>
+                  <input type="text" className="form-control" id="description" name="description" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="tag" className="form-label">Tag</label>
+                  <input type="text" className="form-control" id="tag" name="tag" />
+                </div>
+              </form>
+
 
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
+              <button type="button" className="btn btn-primary">Update Note </button>
             </div>
           </div>
         </div>
