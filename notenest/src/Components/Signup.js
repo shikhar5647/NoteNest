b482461206d82112b8a9e94c51b010a67eea8333
Shikhar Dave <b22ch032@iitj.ac.in>
2025-04-06 14:45:14 +0530

    better

--- notenest/src/Components/Signup.js
+++ notenest/src/Components/Signup.js
@@ -1,26 +1,36 @@
 import React from 'react'
+import { useState } from 'react';
+import {useHistory} from 'react-router-dom';
 
 const Signup = () => {
+    const [credentials, setCredentials] = useState({name:"", email:"", password:"", cpassword:""});
+    const onChange = (e) => {
+        console.log(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submitted");
+    }
     return (
         <div className='container'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Name </label>
-                    <input type="text" className="form-control" id="name" onChange={onChange} aria-describedby="emailHelp"/>
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    <input type="text" className="form-control" id="name" name='name' onChange={onChange} aria-describedby="emailHelp"/>
+                        <div id="emailHelp" className="form-text">We'll never share your Name  with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="text" className="form-control" id="email" onChange={onChange} aria-describedby="emailHelp"/>
+                    <input type="text" className="form-control" id="email" name='email' onChange={onChange} aria-describedby="emailHelp"/>
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" onChange={onChange} />
+                    <input type="password" className="form-control" id="password" name='password' onChange={onChange} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-                    <input type="password" className="form-control" id="cpassword" onChange={onChange} />
+                    <input type="password" className="form-control" id="cpassword" name='cpassword' onChange={onChange} />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
