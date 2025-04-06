7af223bfb4aa175c256fcb0ce6706186113a6cda
Shikhar Dave <b22ch032@iitj.ac.in>
2025-04-06 10:12:26 +0530

    better

--- notenest/src/Components/login.js
+++ notenest/src/Components/login.js
@@ -3,23 +3,32 @@ import React from 'react'
 const login = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
+        fetch('http://localhost:5000/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        }).then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
     }
     return (
         <div>
-            <form>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" name='email' aria-describedby="emailHelp"/>
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp"/>
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" name='password' />
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" name='password' />
                 </div>
-                <button type="submit" class="btn btn-primary" onSubmit={handleSubmit}>Submit</button>
+                <button type="submit" className="btn btn-primary" >Submit</button>
             </form>
         </div>
     )
