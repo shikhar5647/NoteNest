8bcfdd2b2ff3843b57454a23b479e5ad12777e01
Shikhar Dave <b22ch032@iitj.ac.in>
2025-04-06 09:57:18 +0530

    handle_submit

--- notenest/src/Components/login.js
+++ notenest/src/Components/login.js
@@ -1,23 +1,25 @@
 import React from 'react'
 
 const login = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
     return (
         <div>
             <form>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" name='email' aria-describedby="emailHelp"/>
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1"/>
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name='password' />
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary" onSubmit={handleSubmit}>Submit</button>
             </form>
         </div>
     )
